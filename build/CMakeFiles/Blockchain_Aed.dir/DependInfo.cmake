a1f32a3840f6826345fbbf57dfced1354be5c018
Sebastian-Loza05 <sebastian.loza@utec.edu.pe>
2023-05-07 06:12:57 +0000

    Libreria de interfaz grafica

---
+++ build/CMakeFiles/Blockchain_Aed.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sebastian/OneDrive/Documentos/Universidad/Ciclo_5/Aed/Blockchain_Aed/frames.cpp" "CMakeFiles/Blockchain_Aed.dir/frames.cpp.o" "gcc" "CMakeFiles/Blockchain_Aed.dir/frames.cpp.o.d"
+  "/home/sebastian/OneDrive/Documentos/Universidad/Ciclo_5/Aed/Blockchain_Aed/main.cpp" "CMakeFiles/Blockchain_Aed.dir/main.cpp.o" "gcc" "CMakeFiles/Blockchain_Aed.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Blockchain_Aed.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Blockchain_Aed"
+  "Blockchain_Aed.pdb"
+  "CMakeFiles/Blockchain_Aed.dir/frames.cpp.o"
+  "CMakeFiles/Blockchain_Aed.dir/frames.cpp.o.d"
+  "CMakeFiles/Blockchain_Aed.dir/main.cpp.o"
+  "CMakeFiles/Blockchain_Aed.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blockchain_Aed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
